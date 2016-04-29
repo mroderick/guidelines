93743ac36b89d52b94b171821e7cc3e25506a6b7
Jonathan Kim <jkimbo@gmail.com>
2016-04-29 15:41:46 +0100

    Run eslint on the rules (meta!)

--- styleguides/eslint-config-lystable/index.js
+++ styleguides/eslint-config-lystable/index.js
@@ -1,43 +1,53 @@
+// see http://eslint.org/docs/user-guide/configuring.html#configuring-rules
+const OFF = 0;
+const WARNING = 1; // eslint-disable-line no-unused-vars
+const ERROR = 2;
+
 module.exports = {
-  "plugins": ["react"],
+  'plugins': ['react', 'import'],
   // Extend AirBnB style guide - https://github.com/airbnb/javascript
-  "extends": "eslint-config-airbnb",
+  'extends': 'eslint-config-airbnb',
+  'env': {
+    'browser': false,
+  },
   // on top of the AirBnB guide...
-  "rules": {
-    "no-use-before-define": [2, "nofunc"],
-    "no-console": 2,
-    "no-loop-func": 0,
-    "indent": [2, 2, {"SwitchCase": 1}],
-    "quotes": [2, "single", "avoid-escape"],
-    "no-nested-ternary": 0,
-    "padded-blocks": 0,
-    "space-infix-ops": 0,
-    "object-curly-spacing": [2, "never"],
-    "array-bracket-spacing": [2, "never"],
-    "computed-property-spacing": [2, "never"],
-    "space-in-brackets": 0,
-    "space-before-function-paren": [2, { "anonymous": "never", "named": "never" }],
-    "quote-props": 0, // prefer but allow consistency
-    "no-unneeded-ternary": [2, {"defaultAssignment": true}],
+  'rules': {
+    'no-use-before-define': [ERROR, 'nofunc'],
+    'no-console': ERROR,
+    'no-loop-func': OFF,
+    'indent': [ERROR, 2, {'SwitchCase': 1}],
+    'quotes': [ERROR, 'single', 'avoid-escape'],
+    'no-nested-ternary': OFF,
+    'padded-blocks': OFF,
+    'space-infix-ops': OFF,
+    'object-curly-spacing': [ERROR, 'never'],
+    'array-bracket-spacing': [ERROR, 'never'],
+    'computed-property-spacing': [ERROR, 'never'],
+    'space-in-brackets': OFF,
+    'space-before-function-paren': [ERROR, {'anonymous': 'never', 'named': 'never'}],
+    'quote-props': OFF, // prefer but allow consistency
+    'no-unneeded-ternary': [ERROR, {'defaultAssignment': true}],
+    'max-len': OFF,
     // react rules - https://github.com/yannickcr/eslint-plugin-react
-    "react/no-multi-comp": [2, {"ignoreStateless": true}],
-    "react/jsx-max-props-per-line": 0,
-    "react/jsx-no-duplicate-props": 2,
-    "react/jsx-pascal-case": 2,
-    "react/no-direct-mutation-state": 2,
-    "react/jsx-key": 2,
-    "react/prop-types": 2,
-    "react/jsx-indent": [2, 2],
-    "react/jsx-indent-props": [2, 2],
-    "react/jsx-boolean-value": [2, "always"],
-    "react/jsx-closing-bracket-location": [2, "tag-aligned"],
+    'react/no-multi-comp': [ERROR, {'ignoreStateless': true}],
+    'react/jsx-max-props-per-line': OFF,
+    'react/jsx-no-duplicate-props': ERROR,
+    'react/jsx-pascal-case': ERROR,
+    'react/no-direct-mutation-state': ERROR,
+    'react/jsx-key': ERROR,
+    'react/prop-types': ERROR,
+    'react/jsx-indent': [ERROR, 2],
+    'react/jsx-indent-props': [ERROR, 2],
+    'react/jsx-boolean-value': [ERROR, 'always'],
+    'react/jsx-closing-bracket-location': [ERROR, 'tag-aligned'],
     // TODO: rules which aren't quite working right...
-    "react/display-name": [0, {"acceptTranspilerName": true}], // captures any function returning jsx
+    'react/display-name': [OFF, {'ignoreTranspilerName': false}], // captures any function returning jsx
     // TODO: rules to roll in over time...
-    "comma-dangle": [0, "always-multiline"],
-    "react/jsx-handler-names": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-no-bind": 0,
-    "react/no-string-refs": 0
+    'comma-dangle': [OFF, 'always-multiline'],
+    'react/jsx-handler-names': OFF,
+    'react/forbid-prop-types': OFF,
+    'react/jsx-no-bind': OFF,
+    'react/no-string-refs': OFF,
+    'react/prefer-stateless-function': OFF,
   }
 };
